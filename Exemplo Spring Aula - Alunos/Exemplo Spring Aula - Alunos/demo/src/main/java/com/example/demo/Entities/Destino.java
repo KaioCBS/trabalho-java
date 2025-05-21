5b0d55811af95867e85f5806f8666c607621ade4
kaio <kaiocesarsimoes@hotmail.com>
2025-05-21 20:24:16 -0300

    Entidades Kaio

---
+++ Exemplo Spring Aula - Alunos/Exemplo Spring Aula - Alunos/demo/src/main/java/com/example/demo/Entities/Destino.java
@@ -0,0 +1,38 @@
+package com.example.demo.Entities;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Data
+@Entity
+@Table(name = "Destino")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Destino {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+    @Column(nullable = false)
+    private String descricao;
+    @Column(nullable = false)
+    private String localizacao;
+    @Column(nullable = false)
+    private float precoporpessoa;
+
+}
