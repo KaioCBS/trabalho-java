28048c8408c3211dd46a2d57fc53cd2d7c454a07
kaio <kaiocesarsimoes@hotmail.com>
2025-06-08 21:46:23 -0300

    Criar Destino Funcinando

--- Exemplo Spring Aula - Alunos/Exemplo Spring Aula - Alunos/demo/src/main/java/com/example/demo/dto/DestinoDTO.java
+++ Exemplo Spring Aula - Alunos/Exemplo Spring Aula - Alunos/demo/src/main/java/com/example/demo/dto/DestinoDTO.java
@@ -1,22 +1,18 @@
 package com.example.demo.dto;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-<<<<<<< HEAD
-import jakarta.validation.constraints.Positive;
-=======
->>>>>>> 944e750ea295c808f1837f2ee5d96a6be2d53685
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 
 @Data
 @NoArgsConstructor
 public class DestinoDTO {
-
     private Long id;
-<<<<<<< HEAD
-    
-   @NotBlank(message = "Descrição é obrigatória")
+
+    @NotBlank(message = "Descrição é obrigatória")
     private String descricao;
 
     @NotNull(message = "Preço por pessoa é obrigatório")
@@ -25,22 +21,7 @@ public class DestinoDTO {
 
     @NotBlank(message = "Nome é obrigatório")
     private String nome;
-    
+
     @NotBlank(message = "Localização é obrigatória")
     private String localizacao;
-}
-=======
-
-    @NotBlank(message = "O nome é obrigatório")
-    private String nome;
-
-    @NotBlank(message = "A descrição é obrigatória")
-    private String descricao;
->>>>>>> 944e750ea295c808f1837f2ee5d96a6be2d53685
-
-    @NotBlank(message = "A localização é obrigatória")
-    private String localizacao;
-
-    @NotNull(message = "O preço por pessoa é obrigatório")
-    private Double precoPorPessoa;
 }
